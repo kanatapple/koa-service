1a59afa2e3cadbee2e1df3ac96a32513c9f939d3
Yasunari Fujieda <edamame1224@gmail.com>
2016-12-07 18:49:59 +0900

    update .gitignore, app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,30 @@
-const app = require('koa')();
+'use strict';
 
+const app = require('koa')();
+const route = require('koa-route');
+
+app.use(route.get('/', function* () {
+    this.body = `
+<h1>Index Page</h1>
+<a href="/about">To About</a>
+<a href="/help">To Help</a>
+`;
+}));
+
+app.use(route.get('/about', function* () {
+    this.body = `
+<h1>About Page</h1>
+<a href="/">To Index</a>
+<a href="/help">To Help</a>
+`;
+}));
+
+app.use(route.get('/help', function* () {
+    this.body = `
+<h1>Help Page</h1>
+<a href="/">To Index</a>
+<a href="/about">To About</a>
+`;
+}));
+
+app.listen(3000);
