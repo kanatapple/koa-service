c6bf538d3e3988a34a42aed8ea7b4a8c5ca02a40
kanatapple <edamame1224@gmail.com>
2016-12-23 17:18:45 +0900

    update register.js

--- docs/register.js
+++ docs/register.js
@@ -1,7 +1,22 @@
 'use strict';
 
 if (navigator.serviceWorker) {
+    const controllerChange = new Promise((resolve) => {
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+            resolve(navigator.serviceWorker.controller);
+        });
+    });
+    
     navigator.serviceWorker.register('./sw.js', {scope: './'})
+             .then(() => {
+                 return navigator.serviceWorker.ready;
+             })
+             .then(() => {
+                 if (navigator.serviceWorker.controller) {
+                     return navigator.serviceWorker.controller;
+                 }
+                 return controllerChange;
+             })
              .then(() => {
                  location.reload();
              })
